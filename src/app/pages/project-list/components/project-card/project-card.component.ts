2a06b63c45b33cd011854aad56a18bad504c8285
MrWhite <hugoblanc.blend@gmail.com>
2020-04-13 12:23:12 +0200

    feat: basic project card template

--- src/app/pages/project-list/components/project-card/project-card.component.ts
+++ src/app/pages/project-list/components/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from '../../../../models/project';
 
 @Component({
   selector: 'wpj-project-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-card.component.scss']
 })
 export class ProjectCardComponent implements OnInit {
+
+@Input() project: Project;
 
   constructor() { }
 
--- src/app/pages/project-list/project-list.component.ts
+++ src/app/pages/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'wpj-project-list',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
+  projects: Project[] = [];
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.projects.push(new Project({
+      id: 1,
+      name: 'NeuralJS',
+      githubLink: 'https://github.com/hugoblanc/NeuralJS',
+      imageUrl: 'https://miro.medium.com/max/6000/1*wT6pIMnjZ9oArkidnVsGtg.png'
+    }));
   }
 
 }
