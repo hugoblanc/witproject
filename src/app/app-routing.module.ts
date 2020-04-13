4101ebdb26ac04ece272e9019b93f45b4b277ba0
MrWhite <hugoblanc.blend@gmail.com>
2020-04-13 12:07:51 +0200

    feat: navigation to project list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectListComponent } from './pages/project-list/project-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ProjectListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
