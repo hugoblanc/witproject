617e0899424c4fee41147fd01ac72929cd08a0e4
MrWhite <hugoblanc.blend@gmail.com>
2020-04-13 12:47:11 +0200

    feat(project-details): receive data from url

--- src/app/pages/project-details/project-details.component.ts
+++ src/app/pages/project-details/project-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'wpj-project-details',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailsComponent implements OnInit {
 
-  constructor() { }
+  project: Project;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe((paramMap) => {
+      const projects = [];
+      projects.push(new Project({
+        id: 1,
+        name: 'NeuralJS',
+        githubLink: 'https://github.com/hugoblanc/NeuralJS',
+        imageUrl: 'https://miro.medium.com/max/6000/1*wT6pIMnjZ9oArkidnVsGtg.png'
+      }));
+
+
+      const idStr = paramMap.get('id');
+      const id = parseInt(idStr, 10);
+      if (Number.isNaN(id)) {
+        this.router.navigateByUrl('');
+      }
+
+
+      this.project =  projects.find((p: Project) => p.id === id);
+
+    });
   }
 
 }
