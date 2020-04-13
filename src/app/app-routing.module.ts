d8bef3f4b3f3d46ef82431a3b3591e12ce779a48
MrWhite <hugoblanc.blend@gmail.com>
2020-04-13 12:27:50 +0200

    feat: project details added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 
 
 const routes: Routes = [
-  {path:'', component: ProjectListComponent}
+  { path: '', component: ProjectListComponent },
+  { path: 'project-details/:id', component: ProjectDetailsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
 import { ProjectCardComponent } from './pages/project-list/components/project-card/project-card.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectListComponent,
-    ProjectCardComponent
+    ProjectCardComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
