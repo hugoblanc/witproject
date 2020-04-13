20a004c1eff16e3a239280b9dec41a3273466cff
MrWhite <hugoblanc.blend@gmail.com>
2020-04-13 12:56:47 +0200

    fix(project-details):forget hardcoded project 2

--- src/app/pages/project-list/project-list.component.ts
+++ src/app/pages/project-list/project-list.component.ts
@@ -20,6 +20,11 @@ export class ProjectListComponent implements OnInit {
       name: 'NeuralJS',
       githubLink: 'https://github.com/hugoblanc/NeuralJS',
       imageUrl: 'https://miro.medium.com/max/6000/1*wT6pIMnjZ9oArkidnVsGtg.png'
+    }), new Project({
+      id: 2,
+      name: 'Athena',
+      githubLink: 'https://github.com/hugoblanc/Athena',
+      imageUrl: 'https://avatars3.githubusercontent.com/u/15015179?s=460&u=62c173794efcef6d92e06eb4c648fef3369c4598&v=4'
     }));
   }
 
