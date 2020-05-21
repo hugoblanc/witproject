3687c93831096d5c12489f182490a8ce314144c7
MrWhite <hugoblanc.blend@gmail.com>
2020-05-21 17:23:30 +0200

    feat: add compodoc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+/**
+ * App component
+ */
 @Component({
   selector: 'wpj-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  /**
+   * Page's title
+   */
   title = 'witprojects';
 }
