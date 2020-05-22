576c26bba675a31628820036992de1be9dc52620
MrWhite <hugoblanc.blend@gmail.com>
2020-05-22 20:26:49 +0200

    refactor (details): use service getById

--- src/app/pages/project-details/project-details.component.ts
+++ src/app/pages/project-details/project-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from '../../models/project';
+import { ProjectService } from '../../shared/project.service';
 
 @Component({
   selector: 'wpj-project-details',
@@ -11,35 +12,28 @@ export class ProjectDetailsComponent implements OnInit {
 
   project = new Project();
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private projectService: ProjectService) { }
 
   ngOnInit(): void {
 
     this.route.paramMap.subscribe((paramMap) => {
-      const projects = [];
-      projects.push(new Project({
-        id: 1,
-        name: 'NeuralJS',
-        githubLink: 'https://github.com/hugoblanc/NeuralJS',
-        imageUrl: 'https://miro.medium.com/max/6000/1*wT6pIMnjZ9oArkidnVsGtg.png'
-      }), new Project({
-        id: 2,
-        name: 'Athena',
-        githubLink: 'https://github.com/hugoblanc/Athena',
-        imageUrl: 'https://avatars3.githubusercontent.com/u/15015179?s=460&u=62c173794efcef6d92e06eb4c648fef3369c4598&v=4'
-      }));
-
-
       const idStr = paramMap.get('id');
       const id = parseInt(idStr, 10);
+
       if (Number.isNaN(id)) {
-        this.router.navigateByUrl('');
+        this.goToHome();
       }
 
+      this.project = this.projectService.getById(id);
 
-      this.project = projects.find((p: Project) => p.id === id);
-
+      if (!this.project) {
+        this.goToHome();
+      }
     });
+  }
+
+  private goToHome(): void {
+    this.router.navigateByUrl('');
   }
 
 }
